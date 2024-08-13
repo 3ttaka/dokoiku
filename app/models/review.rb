a0db1511a3df148019925d9ade6c5aa7ca9546f4
3ttaka <s3ttaka0723@gmail.com>
2024-08-13 13:56:40 +0900

    画像保存

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :shop
   belongs_to :user
+  has_many_attached :images
 
   validates :cleanliness, :space, :lighting, :music, :vibrancy,
             :order_speed, :service_style, :conversation,
