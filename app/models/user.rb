a166b3e23df63056f56dcbe1886794d2d2c0a96e
3ttaka <s3ttaka0723@gmail.com>
2024-08-20 12:33:01 +0900

    マイページ詳細

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :reviews
+
   validates :nickname, presence: true
   validates :gender_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :age_group_id, numericality: { other_than: 1, message: "can't be blank" }
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
     end
     resources :reviews, only: [:new, :create, :show]
   end
+  resources :users, only: :show
 end
